52ae3b2a4df43f858d1d5c1a1c49ababb4f46810
Joe Doyle <65048714+JoeDoyle12@users.noreply.github.com>
2021-01-20 14:58:07 -0500

    Update headers.cpp

--- headers.cpp
+++ headers.cpp
@@ -1,4 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+//  You're not going to master the rest of your life in one day.
+//  Just relax. Master the day. 
+//  Then just keep doing that every day.
+#include <bits/stdc++.h>
 using namespace std;
+#define f first
+#define s second
+#define FOR(i,n) for(int i=0;i<(int)(n);i++)
+#define FOR1(i,n) for(int i=1;i<=(int)(n);i++)
+#define RFOR(i,n) for(int i=(int)(n)-1;i>=0;i--)
+#define RFOR1(i,n) for(int i=(int)(n);i>0;i--)
+#define FORRNG(i, start, end) for (int i = start; i < end; ++i)
+#define vec vector<int>
+#define put push_back
+#define ll long long
+#define len(x) x.size()
+#define s(x) sort(x.begin(), x.end())
+#define all(x) x.begin(),x.end()
+#define lb(a, b, c) lower_bound(a, b, c)
+#define ub(a, b, c) upper_bound(x, b, c)
